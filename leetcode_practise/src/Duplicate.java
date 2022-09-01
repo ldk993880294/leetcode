eabc4add2dab520c2553546b13807ad21e97230c
ldk993880294 <ldk2016@163.com>
2022-09-01 23:17:40 +0800

    22 09 01 将一个字符串中的空格替换成 "%20"

--- leetcode_practise/src/Duplicate.java
+++ leetcode_practise/src/Duplicate.java
@@ -5,14 +5,13 @@ public class Duplicate {
     }
 
     private static int duplicate(int[] input) {
-        int res=0;
         for(int i=0;i<input.length;i++){
             for(int j=i+1;j<input.length;j++){
                 if(input[i]==input[j]){
-                    res=input[i];
+                    return input[i];
                 }
             }
         }
-        return res;
+        return -1;
     }
 }
---
+++ leetcode_practise/src/ReplaceSpace.java
@@ -0,0 +1,27 @@
+public class ReplaceSpace {
+    public static void main(String[] args) {
+        StringBuffer input=new StringBuffer("A B");
+        System.out.println(replaceSpace(input));
+    }
+
+    private static String replaceSpace(StringBuffer input) {
+        int P1=input.length()-1;
+        for(int i=0;i<input.length();i++){
+            if(input.charAt(i)==' '){
+                input.append("  ");
+            }
+            int P2=input.length()-1;
+            while(P1>0&&P2>P1){
+                char c=input.charAt(P1--);
+                if(c==' '){
+                    input.setCharAt(P2--,'0');
+                    input.setCharAt(P2--,'2');
+                    input.setCharAt(P2--,'%');
+                }else{
+                    input.setCharAt(P2--,c);
+                }
+            }
+        }
+        return input.toString();
+    }
+}
